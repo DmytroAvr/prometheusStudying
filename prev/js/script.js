2b8f7500c1dbd9dad198e545b5053ab390ea04e1
Di <dmytro9avr@gmail.com>
2022-09-23 20:09:23 +0300

    gf

---
+++ prev/js/script.js
@@ -0,0 +1,35 @@
+
+//add and?deleteEventLinstener
+
+//const add = document.querySelector(".add");
+
+//const del = document.querySelector('.delete');
+
+//	const message = function() {
+//		console.log("hello. My name is log");
+//}
+	
+//add.addEventListener("click", message);
+//add.addEventListener("click", () => {
+//	console.log('second line');
+//});
+
+//del.addEventListener("click", () => {
+//	add.removeEventListener("click", message);
+//});
+//remove only if in *okrema* function
+
+
+//*************************************************************/
+
+// add event listiner for all button. You can push button only once
+
+//const buttons = document.querySelectorAll("button");
+
+//const clicked = function (e) {
+//	e.target.disabled = true;
+//	console.log('clicked', e);	
+//}
+//buttons.forEach((btn) => {
+//	btn.addEventListener("click", clicked);
+//});
